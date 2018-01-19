b8b0bb091d55ffba95797de78303c281b60f30a2
Dhruv Singal <dhruvsingalabc@gmail.com>
2018-01-18 23:17:32 -0800

    Add files via upload

--- 357/hw1/mytr.c
+++ 357/hw1/mytr.c
@@ -4,8 +4,9 @@
 #include<string.h>
 
 int main(int argc, char* argv[]){
-   check_arguments();
+   int result;
+   result=check_arguments();
 }
-void check_arguments(int num_arguments, char* argument_array[]){
-	if(argument_array[0]=="-d")
+int check_arguments(int num_arguments, char* argument_array[]){
+	if(arg
 }
