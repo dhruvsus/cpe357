69342d379d50b124e95c9c2590a94bf4a93210ef
Dhruv Singal <dhruvsingalabc@gmail.com>
2018-01-19 00:59:26 -0800

    making some progress with the delete

--- hw1/mytr.c
+++ hw1/mytr.c
@@ -5,8 +5,52 @@
 
 int main(int argc, char* argv[]){
    int result;
+   int delete=0;
    result=check_arguments();
+   if(result==1){
+       return result;
+   }
+   else if(strcmp(argv[1],"-d")==0){
+       delete=1;
+   }
+   init_array(arr);
+   if(delete){
+       delete_tr(argv);
+   }
+   else{
+       replace_tr(argv);
+   }
 }
 int check_arguments(int num_arguments, char* argument_array[]){
-	if(arg
+	if(num_arguments<3){
+        printf("tr: missing operand\nTry 'tr --help' for more information.\n");
+        return 1;
+    }
+    else if(strcmp(argument_array[1],"-d")==0 && num_arguments>3){
+        printf("tr: extra operand '%s'\nOnly one string may be given when deleting without squeezing repeats.\nTry 'tr --help' for more information.\n", argv[3]);
+        return 1;
+    }
+    else if(num_arguments>3){
+        printf("tr: extra operand ‘%s’\nTry 'tr --help' for more information.\n", argument_array[3]);
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+void init_array(int* arr){
+    int i=0;
+    for(i=0;i<256;i++){
+        arr[i]=(int)(i);
+    }
+}
+void delete_tr(char* argument_array[]){
+    char* del_set=argument_array[2];
+    while(*del_set!='\0'){
+        if(*del_set=='\' && *del_set!='\0'){
+            switch(*(del_set+1)){
+                
+            }
+        }
+    }
 }
--- hw1/mytr.h
+++ hw1/mytr.h
@@ -1,4 +1,8 @@
 #ifndef MYTR_H
 #define MYTR_H
-void checkarguments(int num_arguments, char* argument_array[]);
+int checkarguments(int num_arguments, char* argument_array[]);
+void delete_tr(char* argv[]);
+void replace_tr(char* argv[]);
+int arr[256];
+void init_array(int* arr);
 #endif
