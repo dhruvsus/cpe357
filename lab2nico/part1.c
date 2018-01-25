d5d189a836195549de627901a2be53c991f8d797
Dhruv Singal <dsingal@localhost.localdomain>
2018-01-25 14:47:29 -0800

    dded files for lab2 by nico

---
+++ lab2nico/part1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(int argc, char* argv[]){
+    char ch='h';
+    char* cp;
+    cp=&ch;
+    char apString[10]="hello";
+    char* ap=apString;
+    int ppPointer=5;
+    int* pp=&ppPointer;
+}
