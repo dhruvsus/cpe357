cc9066189dadd82728659f8c470264cb5a66f789
Dhruv Singal <dhruvsingalabc@gmail.com>
2018-02-07 02:06:24 -0800

    started 2

--- hw3/hencode.c
+++ hw3/hencode.c
@@ -4,3 +4,15 @@
 #include<stdio.h>
 #include "hencode.h"
 #include<stdlib.h>
+#include<errno.h>
+
+int main(int argc, char* argv[]){
+    if(argc<2){
+        perror(usage());
+        exit(EXIT_FAILURE);
+    }
+}
+char* usage(){
+    char* usage_error="usage: ./hencode infile [ outfile ]";
+    return usage_error;
+}
--- hw3/hencode.h
+++ hw3/hencode.h
@@ -4,5 +4,5 @@
 
 #ifndef HW3_HENCODE_H
 #define HW3_HENCODE_H
-
+char* usage();
 #endif //HW3_HENCODE_H
