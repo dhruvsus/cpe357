64b5b73c39eb70440eef1e89e6e148cf929f19b9
DhruvSUS <dhruvsingalabc@gmail.com>
2018-03-02 14:07:15 -0800

    started timer

--- lab6/timer.c
+++ lab6/timer.c
@@ -1,14 +1,20 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<time.h>
 #include<string.h>
+#include<sys/time.h>
+#include"timer.h"
 int main(int argc, char* argv[]){
     int len,i;
     if(argc>1 || argc ==0){
         timeerrusage();
     }
     else if(argc==1){
-        if(atoi(argv[1])==0)
- 
+        if(atoi(argv[1])==0){
+          timeerrusage();
+        }
     }
+}
+void timeerrusage(){
+  perror(" ");
+  exit(1);
 }
